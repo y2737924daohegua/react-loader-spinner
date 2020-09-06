e1f1286749220636d961914995a4908a815d6258
= <=>
2020-09-06 17:16:14 +0545

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Component() {
+  return /*#__PURE__*/_react["default"].createElement("p", null, "Hello");
+}
+
+var _default = Component;
+exports["default"] = _default;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Component() {
+  return <p>Hello</p>;
+}
+
+export default Component;
